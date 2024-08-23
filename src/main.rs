bf1824cabede63f98a73fe9e91cf17f62d6d4e63
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-08-22 20:32:26 -0700

    feat: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+mod analyzer;
+mod parser;
+mod plotter;
+
+use std::path::PathBuf;
+
+use anyhow::Result;
+use clap::Parser;
+use parser::ConversationDirectory;
+
+#[derive(Parser, Debug)]
+#[command(version, author, about, long_about = None)]
+struct Args {
+    #[arg(short, long, help = "input directory containing message json files")]
+    path: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let analysis = ConversationDirectory::try_from(args.path)
+        .unwrap()
+        .parse()?
+        .analyze();
+
+    // if it doesn't already exist, create the output directory
+    let output_dir = "./output";
+    if !PathBuf::from(output_dir).exists() {
+        std::fs::create_dir(output_dir)?;
+    }
+
+    // generate every plot
+    for plot_type in [
+        plotter::PlotType::Positive,
+        plotter::PlotType::Negative,
+        plotter::PlotType::Neutral,
+        plotter::PlotType::Compound,
+    ] {
+        analysis.plot(
+            plot_type,
+            &PathBuf::from(format!("{output_dir}/{plot_type}.png")),
+        )?;
+    }
+
+    Ok(())
 }
